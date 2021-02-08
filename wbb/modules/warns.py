e496f420cdb3426c013b89331dd9707c41567e1f
Programming Error <75001577+ProgrammingError@users.noreply.github.com>
2021-02-08 19:21:58 +0530

    Create warns.py

---
+++ wbb/modules/warns.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+from pyrogram.types import Message, ChatPermissions
+from wbb import OWNER_ID, SUDO_USER_ID, app
+from wbb.utils import cust_filter
+from wbb.utils.botinfo import BOT_ID
+from pyrogram.types import (
+    CallbackQuery,
+    Chat,
+    ChatPermissions,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    User,
+)
+
