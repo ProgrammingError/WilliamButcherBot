108eb4ccaef1e3c97c2a6c8be453574a43cacfaa
Programming Error <75001577+ProgrammingError@users.noreply.github.com>
2021-02-10 17:06:10 +0530

    Update __init__.py

--- wbb/modules/sql/__init__.py
+++ wbb/modules/sql/__init__.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
-from wbb import DB_URI
+from wbb import DB
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(DB, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
